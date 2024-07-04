217da7eca95703559fc9f4d6a84eb4806117ee75
Ayush maurya <Ayushmaurya20004@gmail.com>
2024-07-04 18:50:23 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,106 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+   
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    
+   
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+   
+];
+
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Myntra Clone',
+        description: 'A frontend replica of Myntra websites focusing on her interfaces design visual presentation of fashion items',
+        link: 'https://github.com/AyushVinayMaurya/Myntra/tree/main/Myntra%20clone',
+    },
+  
+];
+
---
+++ src/pages/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { skills } from "../constants";
+import CTA from "../components/CTA";
+
+const About = () => {
+  return (
+    <section className="max-container ">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Ayush
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          A Frontend Developer based in India, specializing in technical
+          education through hands through hands-on learning and building
+          applications.
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl " />
+              <div
+                className="btn-front rounded-xl flex justify-center
+                items-center"
+              >
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="py-16">
+        <h3 className="subhead-text">About Myself</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            I am currently learning and exploring frontend development as a
+            future career. If you need any assistance or resources, feel free to
+            ask. I am focusing on various areas and technologies within frontend
+            development to build a solid foundation and enhances my skills.
+          </p>
+        </div>
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
