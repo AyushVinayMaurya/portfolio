0b0b9a45bf5f1f408476581880602b240860d28d
Ayush <ayushmaurya0066@gmail.com>
2025-04-15 21:28:21 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,29 +28,29 @@ import {
 
 export const skills = [
     {
-        imageUrl: css,
-        name: "CSS",
+        imageUrl: html,
+        name: "HTML",
         type: "Frontend",
     },
     
     {
-        imageUrl: git,
-        name: "Git",
-        type: "Version Control",
-    },
-    {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
-    },
-    {
-        imageUrl: html,
-        name: "HTML",
+        imageUrl: css,
+        name: "CSS",
         type: "Frontend",
     },
     {
         imageUrl: javascript,
         name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
         type: "Frontend",
     },
    
@@ -60,17 +60,29 @@ export const skills = [
         type: "Backend",
     },
     {
-        imageUrl: react,
-        name: "React",
+        imageUrl: nextjs,
+        name: "Next.js",
         type: "Frontend",
     },
     
    
     {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
+        imageUrl: typescript,
+        name: "TypeScript",
         type: "Frontend",
     },
+
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    }
    
 ];
 
@@ -84,15 +96,17 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/AyushVinayMaurya',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/ayushmaurya2005/',
     }
 ];
 
+
+  
 export const projects = [
     {
         iconUrl: pricewise,
@@ -101,6 +115,44 @@ export const projects = [
         description: 'I have developed an eCommerce platform dedicated to home decor, named RadiantHomes .The website is built using HTML, CSS, and JavaScript, ensuring a responsive and user-friendly experience.',
         link: 'https://home-decor-bay.vercel.app/',
     },
-  
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'MockMate',
+        description: 'MockMate is an AI-powered platform that offers personalized interview preparation with language selection, question customization, secure multi-user access, realistic mock interviews, and admin tools for management and optimization.',
+        link: 'https://mock-interviewer-five.vercel.app/',
+    },
+    // {
+    //     iconUrl: car,
+    //     theme: 'btn-back-blue',
+    //     name: 'Car Finding App',
+    //     description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+    //     link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    // },
+    // {
+    //     iconUrl: snapgram,
+    //     theme: 'btn-back-pink',
+    //     name: 'Full Stack Instagram Clone',
+    //     description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+    //     link: 'https://github.com/adrianhajdin/social_media_app',
+    // },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    // }
 ];
+
+
+
+
 
